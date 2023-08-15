848415e1ba945995b15bbaaf03d4d7b482968950
Kafi Rashid <root@Kafis-MacBook-Pro.local>
2023-08-15 11:25:40 -0500

    Solution to Problem E

--- KapriFrameworkE-FW/src/main/java/framework/context/Context.java
+++ KapriFrameworkE-FW/src/main/java/framework/context/Context.java
@@ -1,6 +1,7 @@
 package framework.context;
 
 import framework.annotations.Autowired;
+import framework.annotations.Profile;
 import framework.annotations.Service;
 import framework.annotations.Value;
 import framework.util.ConfigFileReader;
@@ -10,13 +11,11 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Set;
+import java.util.*;
 
 public class Context {
     private static Map<String, Object> serviceObjects = new HashMap<>();
+    private String activeProfile;
 
     public void start(Class<?> clazz) {
         try {
@@ -32,6 +31,8 @@ public class Context {
     }
 
     private void performDependencyInjection() {
+        Properties properties = ConfigFileReader.getConfigProperties();
+        activeProfile = properties.getProperty("profile.active");
         try {
             for (Object service: serviceObjects.values()) {
                 Object returnConstructor = performConstructorInjection(service);
@@ -57,7 +58,7 @@ public class Context {
             for (Field field: service.getClass().getDeclaredFields()) {
                 if (field.isAnnotationPresent(Autowired.class)) {
                     Class<?> fieldType = field.getType();
-                    Object instance = getServiceBeanByType(fieldType.getName());
+                    Object instance = getServiceBeanByType(fieldType);
                     field.setAccessible(true);
                     field.set(service, instance);
                 }
@@ -90,7 +91,7 @@ public class Context {
                     Class<?>[] paramTypes = method.getParameterTypes();
                     Object [] instanceOfParams = new Object[paramTypes.length];
                     for (int i = 0 ; i<paramTypes.length; i++) {
-                        instanceOfParams[i] = getServiceBeanByType(paramTypes[i].getName());
+                        instanceOfParams[i] = getServiceBeanByType(paramTypes[i]);
                     }
                     method.invoke(serviceobject,instanceOfParams);
                 }
@@ -106,11 +107,9 @@ public class Context {
             for (Constructor constructor: constructorList) {
                 if (constructor.isAnnotationPresent(Autowired.class)) {
                     Class<?>[] parameterTypes = constructor.getParameterTypes();
-
                     Object[] parameterInstances = new Object[parameterTypes.length];
-
                     for (int i = 0; i < parameterTypes.length; i++) {
-                        parameterInstances[i] = getServiceBeanByType(parameterTypes[i].getName());
+                        parameterInstances[i] = getServiceBeanByType(parameterTypes[i]);
                     }
                     Object serviceClassInstance = (Object) constructor.newInstance(parameterInstances);
                     serviceObjects.put(serviceClassInstance.getClass().getName(), serviceClassInstance);
@@ -123,8 +122,48 @@ public class Context {
         return null;
     }
 
-    private Object getServiceBeanByType(String name) {
-        return serviceObjects.get(name);
+    public Object getServiceBeanByType(Class<?> interfaceClass) {
+        List<Object> objectList = new ArrayList<Object>();
+        try {
+            for (Object theServiceClass : serviceObjects.values()) {
+                Class<?>[] interfaces = theServiceClass.getClass().getInterfaces();
+                for (Class<?> theInterface : interfaces) {
+                    if (theInterface.getName().contentEquals(interfaceClass.getName()))
+                        objectList.add(theServiceClass);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (objectList.size() == 1) return objectList.get(0);
+        if (objectList.size() > 1) {
+            for (Object theObject : objectList) {
+                String profilevalue = theObject.getClass().getAnnotation(Profile.class).value();
+                if (profilevalue.contentEquals(activeProfile)) {
+                    return theObject;
+                }
+            }
+        }
+        try {
+            for (Object theClass : serviceObjects.values()) {
+                if (theClass.getClass().getName().equals(interfaceClass.getName())) {
+                    return theClass;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private boolean isTypeMatch(Object service, String typeName) {
+        Class<?>[] interfaces = service.getClass().getInterfaces();
+        for (Class<?> intf : interfaces) {
+            if (intf.getName().equals(typeName)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public Object getServiceBeanByClass(Class clazz) {
@@ -137,5 +176,15 @@ public class Context {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public void setActiveProfile(String activeProfile) {
+        this.activeProfile = activeProfile;
+    }
+    private boolean isProfileActive(String profileValues) {
+        if (activeProfile == null || activeProfile.isEmpty()) {
+            return true;
+        }
+        return profileValues.equals(activeProfile);
     }
 }
